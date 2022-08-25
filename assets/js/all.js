bcc42850d330a715551a3d5de1ee3abaaf2768e1
LHchien33 <judy263562@gmail.com>
2022-08-25 23:53:53 +0800

    Update 2022-08-25T15:53:51.500Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,20 +1,18 @@
-$(function() {
+"use strict";
+
+$(function () {
   console.log('Hello Bootstrap5');
 });
-
-$(document).ready(function(){
-
+$(document).ready(function () {
   // editor toggle
   $('.editor-customize').hide();
-  $('.editor-toggle').on('click',function(e){
+  $('.editor-toggle').on('click', function (e) {
     e.preventDefault();
     $('.editor-customize').toggle();
-  });
+  }); // sideMenu link switch
 
-  // sideMenu link switch
-  $('.sideMenu-link').on('click',function(){
+  $('.sideMenu-link').on('click', function () {
     $(this).addClass('sideMenu-link-active').parent().siblings().children('.sideMenu-link').removeClass('sideMenu-link-active');
   });
-
-
 });
+//# sourceMappingURL=all.js.map
