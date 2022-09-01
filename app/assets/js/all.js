00172bc2b7999ea1e777fb1028975aaf9d9d5c37
LHchien33 <judy263562@gmail.com>
2022-09-01 12:00:07 +0800

    依回饋調整

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -18,3 +18,9 @@ $(document).ready(function(){
 
 
 });
+
+ClassicEditor
+          .create( document.querySelector( '#editor' ) )
+          .catch( error => {
+              console.error( error );
+          } );
